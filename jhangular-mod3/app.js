cd9b618d0fcab7c70a1c30655090e50c87a16c13
Jay Morreale <jay_morreale@yahoo.com>
2017-01-07 14:29:08 -0500

    moved js logic for search to directive

--- jhangular-mod3/app.js
+++ jhangular-mod3/app.js
@@ -22,32 +22,42 @@ function FoundItems() {
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuCategoriesService) {
   var list = this;
+  list.found = [];
   list.searchTerm = "";
+  list.error = false;
 
   list.searchMenuItems = function () {
-    list.error = false;
+    list.found.length = 0;  // Clear the Array
+    list.error = false;     // Set error to false
 
     if(list.searchTerm == ""){
       list.error = true;
       return;
     }
 
-    var promise = MenuCategoriesService.getMatchedMenuItems();
-
-    promise.then(function (response) {
-      list.found = [];
-      var menuItems = response.data.menu_items;
-
-      for( var i = 0; i < menuItems.length; i++)
-        if(menuItems[i].description.indexOf(list.searchTerm) != -1)
-          list.found.push(menuItems[i]);
-
+    MenuCategoriesService.getMatchedMenuItems(list.searchTerm).then(function (response) {
+      list.found = response;
       if(list.found.length == 0)
         list.error = true;
     })
-    .catch(function (error) {
-      console.log(error);
-    })
+    // list = promise.data.found;
+    // if(list.found.length == 0)
+    //   list.error = true;
+
+    // promise.then(function (response) {
+    //   list.found = [];
+    //   var menuItems = response.data.menu_items;
+    //
+    //   for( var i = 0; i < menuItems.length; i++)
+    //     if(menuItems[i].description.indexOf(list.searchTerm) != -1)
+    //       list.found.push(menuItems[i]);
+    //
+    //   if(list.found.length == 0)
+    //     list.error = true;
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // })
   };
 
   list.onRemove = function (itemIndex) {
@@ -59,12 +69,22 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
-  service.getMatchedMenuItems = function () {
-    var response = $http({
+  service.getMatchedMenuItems = function (searchTerm) {
+    var foundItems=[];
+    return $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
-    });
-    return response;
+    }).then(function (response) {
+      var menuItems = response.data.menu_items;
+
+      for( var i = 0; i < menuItems.length; i++)
+        if(menuItems[i].description.indexOf(searchTerm) != -1)
+          foundItems.push(menuItems[i]);
+      return foundItems;
+    })
+    .catch(function (error) {
+      console.log(error);
+    }) ;
   };
 }
 
