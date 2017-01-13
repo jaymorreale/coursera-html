5af1dfad449e85f49c38000881462f8d696fa368
Jay Morreale <jay_morreale@yahoo.com>
2017-01-12 22:38:08 -0500

    trying to get stats params working on github

--- router-example/src/app/itemslist.controller.js
+++ router-example/src/app/itemslist.controller.js
@@ -5,11 +5,16 @@ angular.module('App')
 .controller('itemsListController', itemsListController);
 
 // 'items' is injected through state's resolve
-itemsListController.$inject = ['items', $stateParams];
-function itemsListController(items, $stateParams) {
+// itemsListController.$inject = ['items', $stateParams];
+// function itemsListController(items, $stateParams) {
+itemsListController.$inject = ['items', $state];
+function itemsListController(items, $state) {
+
   var ctrl = this;
   ctrl.items = items.data.menu_items;
-  ctrl.cat = $stateParams.cat;
+  // ctrl.cat = $stateParams.cat;
+  ctrl.cat = $state.params.cat;
+
 }
 
 })();
